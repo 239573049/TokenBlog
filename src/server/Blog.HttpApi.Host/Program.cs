bd6fd05e02990d79668ca540ca88f412fd4026b2
token <239573049@qq.com>
2023-02-17 18:48:45 +0800

    删除webassembly 增加compose配置
    Signed-off-by: token <239573049@qq.com>

--- src/server/Blog.HttpApi.Host/Program.cs
+++ src/server/Blog.HttpApi.Host/Program.cs
@@ -10,7 +10,7 @@ namespace Blog;
 
 public class Program
 {
-    public async static Task<int> Main(string[] args)
+    public static async Task<int> Main(string[] args)
     {
         Log.Logger = new LoggerConfiguration()
 #if DEBUG
