6be5758f75b5b9f3b8326198f1619e220ee5fea5
token <239573049@qq.com>
2023-02-18 03:49:39 +0800

    修改clientid
    Signed-off-by: token <239573049@qq.com>

--- src/web/Blog.Component/AuthService.cs
+++ src/web/Blog.Component/AuthService.cs
@@ -14,10 +14,11 @@ public class AuthService
     public async Task GitHub()
     {
         var authorize_uri = "https://github.com/login/oauth/authorize";
-        var client_id = "19c959e3a2f19a495905";
 #if DEBUG
+        var client_id = "19c959e3a2f19a495905";
         var redirect_url = "https://blog.tokengo.top/login";
 #else
+        var client_id = "8771a2c32e83bfd7c38f";
         var redirect_url = "http://localhost:5095/login";
 #endif
         await JsRuntime.InvokeVoidAsync("location.replace",
