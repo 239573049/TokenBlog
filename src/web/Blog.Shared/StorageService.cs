c064276d5939d95e856503cc2086fb2169af86e6
Token <239573049@qq.com>
2023-02-20 04:45:36 +0000

    update src/web/Blog.Shared/StorageService.cs.
    Signed-off-by: Token <239573049@qq.com>

--- src/web/Blog.Shared/StorageService.cs
+++ src/web/Blog.Shared/StorageService.cs
@@ -1,11 +1,11 @@
-﻿namespace Blog.Shared
+namespace Blog.Shared
 {
     public class StorageService
     {
         #if DEBUG
         public static string BaseAddress = "https://localhost:44366/";
         #else
-        public static string BaseAddress = "http://api.blog.tokengo.top/";
+        public static string BaseAddress = "http://blog-api/";
         #endif
 
         public static string WebAssemblyBaseAddress { get; set; }
