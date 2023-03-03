8d862d0c6dd85a8adfbadbd933f728aaf3c5bc4d
Token <239573049@qq.com>
2023-03-03 05:10:46 +0000

    update src/web/Blog.Component/AuthService.cs.
    Signed-off-by: Token <239573049@qq.com>

--- src/web/Blog.Component/AuthService.cs
+++ src/web/Blog.Component/AuthService.cs
@@ -16,7 +16,7 @@ public class AuthService
         var authorize_uri = "https://gitee.com/oauth/authorize";
 #if !DEBUG
         var client_id = "9f850a9918d425f7fa5c0cb75957ae6b61282f6c7a36a28960f05b16dc697f09";
-        var redirect_url = "http://124.222.89.53/login";
+        var redirect_url = "http://server.tokengo.top:11000/login";
 #else
         var client_id = "55f3dce429160a074204b93a6dc886a1d764ebb58383fb6e03c33b395aefbe34";
         var redirect_url = "https://blog.tokengo.top/login";
