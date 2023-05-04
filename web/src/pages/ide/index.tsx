ef9b6c9acd5096c67513bd05d7fbd4dc7ba6a451
token <239573049@qq.com>
2023-05-05 01:24:33 +0800

    xg

--- web/src/pages/ide/index.tsx
+++ web/src/pages/ide/index.tsx
@@ -55,20 +55,28 @@ export default class Ide extends Component {
 
         if (first) {
             Blazor.start({
-                loadBootResource: function (type: string, name: any, defaultUri: string, integrity: any) {
-                    return (async function () {
-                        const response = await fetch('https://token-web-ide.oss-cn-shenzhen.aliyuncs.com/' + defaultUri + '.br', { cache: 'no-cache' });
+                loadBootResource: async function (type: string, name: any, defaultUri: string, integrity: any) {
+                    if (type !== 'dotnetjs') {
+                        return (async function () {
+                            const response = await fetch('https://token-web-ide.oss-cn-shenzhen.aliyuncs.com/' + defaultUri + '.br', { cache: 'no-cache' });
+                            if (!response.ok) {
+                                throw new Error(response.statusText);
+                            }
+                            const originalResponseBuffer = await response.arrayBuffer();
+                            const originalResponseArray = new Int8Array(originalResponseBuffer);
+                            const decompressedResponseArray = BrotliDecode(originalResponseArray);
+                            const contentType = type ===
+                                'dotnetwasm' ? 'application/wasm' : 'application/octet-stream';
+                            return new Response(decompressedResponseArray,
+                                { headers: { 'content-type': contentType } });
+                        })();
+                    } else {
+                        const response = await fetch('https://token-web-ide.oss-cn-shenzhen.aliyuncs.com/' + defaultUri, { cache: 'no-cache' });
                         if (!response.ok) {
                             throw new Error(response.statusText);
                         }
-                        const originalResponseBuffer = await response.arrayBuffer();
-                        const originalResponseArray = new Int8Array(originalResponseBuffer);
-                        const decompressedResponseArray = BrotliDecode(originalResponseArray);
-                        const contentType = type ===
-                            'dotnetwasm' ? 'application/wasm' : 'application/octet-stream';
-                        return new Response(decompressedResponseArray,
-                            { headers: { 'content-type': contentType } });
-                    })();
+                        return response;
+                    }
                 },
             }).then(async () => {
                 // 首次初始化
