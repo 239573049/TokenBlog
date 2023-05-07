9677d22c5d475647e3ec3650c60964e2f6e6ef67
token <239573049@qq.com>
2023-05-07 23:48:05 +0800

    修改ide

--- web/src/pages/ide/index.tsx
+++ web/src/pages/ide/index.tsx
@@ -5,7 +5,7 @@ export default class Ide extends Component {
 
     render() {
         return (
-            <iframe style={{height:"100%",width:'100%'}} src='http://web.tokengo.top:8888/'></iframe>
+            <iframe style={{height:"100%",width:'100%'}} src='https://webassembly.tokengo.top:8843/'></iframe>
         )
     }
 }
